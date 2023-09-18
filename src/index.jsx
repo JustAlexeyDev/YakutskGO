1a3394fcd617566f03bb2f77ac8a12b8d7c20b99
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-09-18 17:39:32 +0900

    init

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+// Import React
+import React from "react";
+import ReactDOM from "react-dom";
+// Import Css
+import './Assets/Css/Global.css';
+import './Assets/Css/Reset.css';
+ReactDOM.render(
+  <React.StrictMode>
+    Hello
+  </React.StrictMode>,
+  document.getElementById('root')
+);
