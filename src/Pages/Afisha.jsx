8c28397a8898340c6adc28b08913bf52c3e4a969
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-09-19 13:42:30 +0900

    header afisha

--- src/Pages/Afisha.jsx
+++ src/Pages/Afisha.jsx
@@ -2,12 +2,14 @@
 import AfishaNav from "../Components/AfishaNav";
 import Calendar from "../Components/Calendar";
 import SliderMain from "./ComponentsAfisha/SliderMain";
+import TopSlider from "./ComponentsAfisha/TopSlider";
 const Afisha = () => {
   return(
     <div className="Afisha">
       <AfishaNav />  
       <Calendar />
       <SliderMain />
+      <TopSlider />
     </div>
   );
 }
